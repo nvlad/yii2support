f35af44964c1c3d02e68613e828e75350a2b4506
NVlad <wildvampir@gmail.com>
2016-12-29 11:00:51 +0300

    show autocomplete on enter quotes

--- src/com/yii2framework/completion/views/CompletionContributor.java
+++ src/com/yii2framework/completion/views/CompletionContributor.java
@@ -4,7 +4,10 @@ import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
-import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.ParameterList;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by NVlad on 27.12.2016.
@@ -12,6 +15,11 @@ import com.jetbrains.php.lang.psi.elements.*;
 public class CompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
     public CompletionContributor() {
         extend(CompletionType.BASIC, ElementPattern(), new CompletionProvider());
+    }
+
+    @Override
+    public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
+        return true;
     }
 
     private static ElementPattern<PsiElement> ElementPattern() {
