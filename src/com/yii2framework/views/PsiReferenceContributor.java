d1fac62e6f0846a46f99ac273a0c531e0fe3007f
NVlad <wildvampir@gmail.com>
2017-01-05 16:36:52 +0300

    opening views by reference

---
+++ src/com/yii2framework/views/PsiReferenceContributor.java
@@ -0,0 +1,25 @@
+package com.yii2framework.views;
+
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReferenceRegistrar;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.ParameterList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by NVlad on 02.01.2017.
+ */
+public class PsiReferenceContributor extends com.intellij.psi.PsiReferenceContributor {
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar psiReferenceRegistrar) {
+        psiReferenceRegistrar.registerReferenceProvider(ElementPattern(), new PsiReferenceProvider());
+    }
+
+    private static ElementPattern<PsiElement> ElementPattern() {
+        return PlatformPatterns.psiElement(PsiElement.class)
+                .withParent(PlatformPatterns.psiElement(ParameterList.class)
+                        .withParent(PlatformPatterns.psiElement(MethodReference.class)));
+    }
+}
