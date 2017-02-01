fb8f7f835828d62ee1ac49c7f5fba69c1bebf1dd
NVlad <wildvampir@gmail.com>
2017-02-02 00:17:17 +0300

    small optimizations for ComponentUtil

--- src/com/yii2support/components/ComponentUtil.java
+++ src/com/yii2support/components/ComponentUtil.java
@@ -6,8 +6,8 @@ import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocProperty;
 import com.jetbrains.php.lang.psi.elements.*;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Created by NVlad on 11.01.2017.
@@ -64,22 +64,22 @@ class ComponentUtil {
     }
 
     static Collection<Field> getClassFields(PhpClass phpClass) {
-        ArrayList<Field> result = new ArrayList<>();
+        final HashSet<Field> result = new HashSet<>();
 
-        Collection<Field> fields = phpClass.getFields();
-        Collection<Method> methods = phpClass.getMethods();
+        final Collection<Field> fields = phpClass.getFields();
+        final Collection<Method> methods = phpClass.getMethods();
         for (Field field : fields) {
             if (field.isConstant()) {
                 continue;
             }
 
-            PhpModifier modifier = field.getModifier();
+            final PhpModifier modifier = field.getModifier();
             if (!modifier.isPublic() || modifier.isStatic()) {
                 continue;
             }
 
             if (field instanceof PhpDocProperty) {
-                String setter = "set" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
+                final String setter = "set" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
                 Boolean setterExist = false;
                 for (Method method : methods) {
                     if (method.getName().equals(setter)) {
