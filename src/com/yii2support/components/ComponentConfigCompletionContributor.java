b22140687878866c23dcbd786f7be945985ace84
NVlad <wildvampir@gmail.com>
2017-02-02 00:23:39 +0300

    temp auto completion popup

--- src/com/yii2support/components/ComponentConfigCompletionContributor.java
+++ src/com/yii2support/components/ComponentConfigCompletionContributor.java
@@ -5,9 +5,11 @@ import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
 import com.jetbrains.php.lang.psi.elements.NewExpression;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.yii2support.common.Patterns;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by NVlad on 11.01.2017.
@@ -15,6 +17,15 @@ import com.yii2support.common.Patterns;
 public class ComponentConfigCompletionContributor extends CompletionContributor {
     public ComponentConfigCompletionContributor() {
         extend(CompletionType.BASIC, ElementPattern(), new ComponentConfigCompletionProvider());
+    }
+
+    @Override
+    public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
+        if ((typeChar == '\'' || typeChar == '"') && position.getParent() instanceof ArrayCreationExpression) {
+            return true;
+        }
+
+        return false;
     }
 
     private static ElementPattern<PsiElement> ElementPattern() {
