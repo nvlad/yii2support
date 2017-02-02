73c5fd215a79f8db10241b867084693ad930e585
NVlad <wildvampir@gmail.com>
2017-02-02 19:40:32 +0300

    update path to moved file in render functions

--- src/com/yii2support/views/references/PsiReference.java
+++ src/com/yii2support/views/references/PsiReference.java
@@ -1,7 +1,12 @@
 package com.yii2support.views.references;
 
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReferenceBase;
+import com.intellij.util.IncorrectOperationException;
+import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.yii2support.views.ViewsUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -24,5 +29,41 @@ public class PsiReference extends PsiReferenceBase<PsiElement> {
     @Override
     public Object[] getVariants() {
         return new Object[0];
+    }
+
+    @Override
+    public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
+        final StringLiteralExpression string = (StringLiteralExpression) this.getElement();
+        final PsiDirectory context = ViewsUtil.getContextDirectory(string);
+        final PsiFile file = (PsiFile) element;
+        final PsiElement newValue;
+
+        String fileName = string.getContents();
+        if (fileName.contains("/")) {
+            fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+        }
+        if (!file.getContainingDirectory().equals(context)) {
+            final PsiDirectory root = ViewsUtil.getRootDirectory(string);
+            if (root == null) {
+                return null;
+            }
+
+            PsiDirectory dir = file.getContainingDirectory();
+            while (dir != null && !dir.equals(root)) {
+                fileName = dir.getName() + "/" + fileName;
+                dir = dir.getParent();
+            }
+
+            if (dir != null && dir.equals(root)) {
+                fileName = "/" + fileName;
+            }
+        }
+        fileName = string.isSingleQuote() ? "'" + fileName + "'" : "\"" + fileName + "\"";
+        newValue = PhpPsiElementFactory.createFromText(element.getProject(), StringLiteralExpression.class, fileName);
+
+        if (newValue != null) {
+            string.replace(newValue);
+        }
+        return newValue;
     }
 }
