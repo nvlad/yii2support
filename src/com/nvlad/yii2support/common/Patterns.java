3d2e61d553c23461153daeecff912604efbadcf2
oleg <oleg@insperedia.com>
2017-03-13 15:27:51 +0100

    1. Lookup in array key 2. Show properties names instead methods

--- src/com/nvlad/yii2support/common/Patterns.java
+++ src/com/nvlad/yii2support/common/Patterns.java
@@ -5,8 +5,8 @@ import com.intellij.patterns.PlatformPatterns;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.injection.PhpElementPattern.Capture;
-import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -26,14 +26,11 @@ public class Patterns extends PlatformPatterns {
         });
     }
 
-    public static Capture<MethodReference> arrayCreation() {
-        return new Capture<>(new InitialPatternCondition<MethodReference>(MethodReference.class) {
+    public static Capture<PhpPsiElement> withHashKey() {
+        return new Capture<>(new InitialPatternCondition<PhpPsiElement>(PhpPsiElement.class) {
             @Override
             public boolean accepts(@Nullable Object o, ProcessingContext context) {
-                if (o instanceof MethodReference) {
-                    return o instanceof ArrayCreationExpression;
-                }
-                return super.accepts(o, context);
+                return o != null && o.toString().equals("Array key");
             }
         });
     }
--- src/com/nvlad/yii2support/objectfactory/ObjectFactoryCompletionContributor.java
+++ src/com/nvlad/yii2support/objectfactory/ObjectFactoryCompletionContributor.java
@@ -5,18 +5,19 @@ import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.PhpLanguage;
 import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.NewExpression;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.jetbrains.php.lang.psi.elements.impl.ArrayCreationExpressionImpl;
 import com.nvlad.yii2support.common.Patterns;
 import org.jetbrains.annotations.NotNull;
 
 public class ObjectFactoryCompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
     public ObjectFactoryCompletionContributor() {
-        extend(CompletionType.BASIC, ElementPattern(), new ObjectFactoryCompletionProvider());
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new ObjectFactoryCompletionProvider());
     }
-
-
 
     @Override
     public boolean invokeAutoPopup(@NotNull PsiElement position, char typeChar) {
@@ -28,6 +29,17 @@ public class ObjectFactoryCompletionContributor extends com.intellij.codeInsight
     }
 
     private static ElementPattern<PsiElement> ElementPattern() {
-        return PlatformPatterns.psiElement().withSuperParent(3, ArrayCreationExpression.class);
+
+        return PlatformPatterns.psiElement()
+                .withParent(PlatformPatterns.psiElement(StringLiteralExpression.class)
+                        .withParent(PlatformPatterns.or(
+                                PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class),
+                                Patterns.withHashKey()
+                                        .withParent(PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class))
+                        )));
+
+
+        // return PlatformPatterns.or(PlatformPatterns.psiElement().withSuperParent(3, ArrayCreationExpression.class),
+        //         PlatformPatterns.psiElement().withSuperParent(4, ArrayCreationExpression.class));
     }
 }
