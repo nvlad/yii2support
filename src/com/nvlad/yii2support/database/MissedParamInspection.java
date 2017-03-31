aa10756ddb51aa37f8ae9586decdde47665be6ca
oleg <oleg@insperedia.com>
2017-03-31 11:40:45 +0200

    Condition params Inspections and quick fix

--- src/com/nvlad/yii2support/database/MissedParamInspection.java
+++ src/com/nvlad/yii2support/database/MissedParamInspection.java
@@ -1,10 +1,12 @@
 package com.nvlad.yii2support.database;
 
 import com.google.common.collect.Lists;
+import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiErrorElement;
 import com.jetbrains.php.lang.inspections.PhpInspection;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
@@ -24,6 +26,32 @@ public class MissedParamInspection extends PhpInspection {
     @Override
     public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean isOnTheFly) {
         return new PhpElementVisitor() {
+
+            @Override
+            public void visitPhpMethodReference(MethodReference reference) {
+                if (reference != null) {
+                    Method method = (Method) reference.resolve();
+                    if (method == null)
+                        return;
+                    if ( method.getParameters().length > 1 &&
+                            method.getParameters()[1].getName().equals("params") &&
+                            method.getParameters()[0].getName().equals("condition")) {
+                        if (reference.getParameters().length == 1 || (reference.getParameters().length > 1 && reference.getParameters()[1] instanceof PsiErrorElement)) {
+                            PsiElement element = reference.getParameters()[0];
+                            if (element instanceof StringLiteralExpression) {
+                                String condition = ((StringLiteralExpression) element).getContents();
+                                String[] conditionParams = DatabaseUtils.extractParamsFromCondition(condition);
+                                if (conditionParams.length > 0) {
+                                    MissedParamQuickFix qFix = new MissedParamQuickFix(reference);
+                                    problemsHolder.registerProblem(reference.getParameters()[0], "Condition parameters must be defined", qFix);
+                                }
+                            }
+                        }
+                    }
+                }
+                super.visitPhpMethodReference(reference);
+            }
+
             @Override
             public void visitPhpArrayCreationExpression(ArrayCreationExpression expression) {
                 MethodReference methodRef = ClassUtils.getMethodRef(expression, 10);
@@ -48,10 +76,11 @@ public class MissedParamInspection extends PhpInspection {
                                 }
 
                                 if (! Arrays.equals(conditionParams, params) ) {
+                                    MissedParamQuickFix qFix = new MissedParamQuickFix(methodRef);
+                                    String problemDesc = "Parameters do not correspond to the condition";
                                     if (params.length == 1)
-                                        problemsHolder.registerProblem(expression, "Parameter does not correspond to the condition");
-                                    else
-                                        problemsHolder.registerProblem(expression, "Parameters do not correspond to the condition");
+                                        problemDesc = "Parameter does not correspond to the condition";
+                                    problemsHolder.registerProblem(expression, problemDesc, qFix);
                                 }
                             }
                         }
