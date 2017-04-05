c26fe66b10f678eb43c7b018150b66de8daff1be
oleg <oleg@insperedia.com>
2017-04-05 13:46:52 +0200

    Tests

--- tests/com/nvlad/yii2support/database/QueryTests.java
+++ tests/com/nvlad/yii2support/database/QueryTests.java
@@ -3,8 +3,8 @@ package com.nvlad.yii2support.database;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.database.psi.DbDataSource;
 import com.intellij.database.psi.DbPsiFacade;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import com.jetbrains.php.lang.PhpFileType;
-import com.nvlad.yii2support.PluginTestCase;
 import com.nvlad.yii2support.database.fixtures.TestDataSource;
 
 import java.io.File;
@@ -13,7 +13,7 @@ import java.util.List;
 /**
  * Created by oleg on 03.04.2017.
  */
-public class QueryTests extends PluginTestCase {
+public class QueryTests  extends LightCodeInsightFixtureTestCase {
 
 
 
@@ -82,8 +82,7 @@ public class QueryTests extends PluginTestCase {
     public void testQuery_Columns() {
 
         myFixture.configureByText(PhpFileType.INSTANCE,   "<?php \n" +
-                " (new \\yii\\db\\Query())->where('person.<caret>\n" +
-                ";");
+                " (new \\yii\\db\\Query())->where('person.<caret>");
         myFixture.completeBasic();
         assertEquals(3, myFixture.getLookupElementStrings().size());
     }
@@ -91,10 +90,11 @@ public class QueryTests extends PluginTestCase {
     public void testQuery_ColumnPrefix() {
 
         myFixture.configureByText(PhpFileType.INSTANCE,   "<?php \n" +
-                " (new \\yii\\db\\Query())->where('person.na<caret>;");
+                " (new \\yii\\db\\Query())->where('person.na<caret>");
         myFixture.completeBasic();
 
-        assertEquals(3, myFixture.getLookupElementStrings().size());
+        assertEquals("name", myFixture.getLookupElementStrings().get(0));
+        assertEquals("surname", myFixture.getLookupElementStrings().get(1));
     }
 
     public void testQuery_ColumnPrefixTableQuoting() {
@@ -110,6 +110,15 @@ public class QueryTests extends PluginTestCase {
 
         myFixture.configureByText(PhpFileType.INSTANCE,   "<?php \n" +
                 " (new \\yii\\db\\Query())->where('{{person}}.[[na<caret>');");
+        myFixture.completeBasic();
+        assertEquals("name", myFixture.getLookupElementStrings().get(0));
+        assertEquals("surname", myFixture.getLookupElementStrings().get(1));
+    }
+
+    public void testQuery_ColumnQuotingAndPrefixTableQuotingDoubleQuote() {
+
+        myFixture.configureByText(PhpFileType.INSTANCE,   "<?php \n" +
+                " (new \\yii\\db\\Query())->where(\"{{person}}.[[na<caret>\");");
         myFixture.completeBasic();
         assertEquals("name", myFixture.getLookupElementStrings().get(0));
         assertEquals("surname", myFixture.getLookupElementStrings().get(1));
