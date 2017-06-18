15ec424cbb6594a88f815cc4f539781310e62281
oleg@insperedia.com <s3cur1ty#First>
2017-06-18 19:12:44 +0200

    Version updated

--- src/com/nvlad/yii2support/validation/ValidationCompletionProvider.java
+++ src/com/nvlad/yii2support/validation/ValidationCompletionProvider.java
@@ -87,7 +87,6 @@ public class ValidationCompletionProvider extends CompletionProvider<CompletionP
                                 }
                             }
 
-
                         }
                     }
                 }
