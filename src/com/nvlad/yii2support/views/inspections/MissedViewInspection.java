79740520c3bcd6c3679643403aa745c91a19dc57
NVlad <wildvampir@gmail.com>
2018-02-20 18:43:01 +0300

    create missed view files

--- src/com/nvlad/yii2support/views/inspections/MissedViewInspection.java
+++ src/com/nvlad/yii2support/views/inspections/MissedViewInspection.java
@@ -8,14 +8,10 @@ import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.indexing.FileBasedIndex;
-import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.inspections.PhpInspection;
-import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
-import com.nvlad.yii2support.common.ClassUtils;
 import com.nvlad.yii2support.common.PhpUtil;
 import com.nvlad.yii2support.common.YiiApplicationUtils;
 import com.nvlad.yii2support.utils.Yii2SupportSettings;
@@ -67,8 +63,8 @@ final public class MissedViewInspection extends PhpInspection {
 
                         final String application = YiiApplicationUtils.getApplicationName(reference.getContainingFile());
                         final boolean localViewSearch;
+                        final String value = PhpUtil.getValue(pathParameter);
                         if (resolve.from == ViewResolveFrom.View) {
-                            final String value = PhpUtil.getValue(pathParameter);
                             localViewSearch = !value.startsWith("@") && !value.startsWith("//");
                         } else {
                             localViewSearch = false;
@@ -85,28 +81,13 @@ final public class MissedViewInspection extends PhpInspection {
                             return;
                         }
 
-                        PhpClass clazz = ClassUtils.getPhpClassByCallChain(reference);
-                        if (clazz != null) {
-                            Method method = clazz.findMethodByName("getViewPath");
-                            PhpIndex phpIndex = PhpIndex.getInstance(reference.getProject());
-                            if (method != null) {
-                                PhpClass containingClass = method.getContainingClass();
-                                PhpClass controllerBaseClass = ClassUtils.getClass(phpIndex, "yii\\base\\Controller");
-                                PhpClass widgetBaseClass = ClassUtils.getClass(phpIndex, "yii\\base\\Widget");
-                                if (containingClass != controllerBaseClass && containingClass != widgetBaseClass) {
-                                    return;
-                                }
-                            }
-                        }
-
+                        Collection<String> paths = ViewUtil.viewResolveToPaths(resolve, project);
                         if (pathParameter instanceof StringLiteralExpression) {
-                            String path = ((StringLiteralExpression) pathParameter).getContents();
-                            final String errorViewNotFoundTemplate = "View file for \"%name%\" not found.";
-                            final MissedViewLocalQuickFix quickFix = new MissedViewLocalQuickFix(path);
-                            final String descriptionTemplate = errorViewNotFoundTemplate.replace("%name%", path);
+                            final String viewNotFoundMessage = "View file for \"" + value + "\" not found.";
+                            final MissedViewLocalQuickFix quickFix = new MissedViewLocalQuickFix(value, paths.iterator().next());
                             final PsiElement stringPart = pathParameter.findElementAt(1);
                             if (stringPart != null) {
-                                problemsHolder.registerProblem(stringPart, descriptionTemplate, quickFix);
+                                problemsHolder.registerProblem(stringPart, viewNotFoundMessage, quickFix);
                             }
                         }
                     }
--- src/com/nvlad/yii2support/views/inspections/MissedViewLocalQuickFix.java
+++ src/com/nvlad/yii2support/views/inspections/MissedViewLocalQuickFix.java
@@ -1,20 +1,28 @@
 package com.nvlad.yii2support.views.inspections;
 
+import com.google.common.io.Files;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
-import com.nvlad.yii2support.views.ViewsUtil;
+import com.intellij.psi.PsiManager;
+import com.jetbrains.php.lang.PhpFileType;
+import com.jetbrains.smarty.SmartyFileType;
+import com.jetbrains.twig.TwigFileType;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -22,9 +30,11 @@ import java.util.Properties;
  */
 class MissedViewLocalQuickFix implements LocalQuickFix {
     final private String myName;
+    final private String myPath;
 
-    MissedViewLocalQuickFix(String name) {
+    MissedViewLocalQuickFix(String name, String path) {
         myName = name;
+        myPath = path;
     }
 
     @Nls
@@ -43,63 +53,83 @@ class MissedViewLocalQuickFix implements LocalQuickFix {
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-        final PsiElement element = descriptor.getPsiElement().getParent();
-        PsiDirectory directory;
-        String path = null;
-
-        if (element instanceof StringLiteralExpression) {
-            path = ((StringLiteralExpression) element).getContents();
-        }
-
-        if (path == null) {
+        final String projectUrl = project.getBaseDir().getUrl();
+        final VirtualFileManager virtualFileManager = VirtualFileManager.getInstance();
+        VirtualFile virtualFile = virtualFileManager.findFileByUrl(projectUrl + myPath);
+        if (virtualFile != null) {
+            System.out.println("File " + projectUrl + myPath + " already exist.");
             return;
         }
 
-        if (path.startsWith("/")) {
-            directory = ViewsUtil.getRootDirectory(element);
-            path = path.substring(1);
-        } else {
-            directory = ViewsUtil.getContextDirectory(element);
-        }
-
+        PsiDirectory directory = PsiManager.getInstance(project).findDirectory(project.getBaseDir());
         if (directory == null) {
             return;
         }
 
-        while (directory != null && path.contains("/")) {
-            final String subdirectory = path.substring(0, path.indexOf('/'));
-            path = path.substring(path.indexOf('/') + 1);
-
-            directory = directory.findSubdirectory(subdirectory);
+        List<String> pathElements = StringUtil.split(StringUtil.trimLeading(myPath, '/'), "/");
+        if (pathElements.size() == 0) {
+            return;
         }
 
-        if (directory != null) {
-            if (!path.contains(".")) {
-                path = path + ".php";
+        final String fileName = pathElements.get(pathElements.size() - 1);
+        pathElements.remove(pathElements.size() - 1);
+        for (String pathElement : pathElements) {
+            if (directory.findSubdirectory(pathElement) == null) {
+                directory.createSubdirectory(pathElement);
             }
-
-            final PsiFile viewPsiFile = directory.createFile(path);
-            FileEditorManager.getInstance(project).openFile(viewPsiFile.getVirtualFile(), true);
-
-            final FileTemplate[] templates = FileTemplateManager.getDefaultInstance().getTemplates(FileTemplateManager.DEFAULT_TEMPLATES_CATEGORY);
-            FileTemplate template = null;
-            for (FileTemplate fileTemplate : templates) {
-                if (fileTemplate.getName().equals("PHP File")) {
-                    template = fileTemplate;
-                    break;
-                }
-            }
-
-            if (template != null && viewPsiFile.getViewProvider().getDocument() != null) {
-                final Properties properties = FileTemplateManager.getDefaultInstance().getDefaultProperties();
-                template.setLiveTemplateEnabled(true);
-                template.setReformatCode(true);
-                try {
-                    viewPsiFile.getViewProvider().getDocument().insertString(0, template.getText(properties));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            directory = directory.findSubdirectory(pathElement);
+            if (directory == null) {
+                return;
             }
         }
+
+        final PsiFile viewPsiFile = directory.createFile(fileName);
+        FileType fileType = FileTypeManager.getInstance().getFileTypeByExtension(Files.getFileExtension(myPath));
+        String templateName = getFileTemplateName(fileType);
+        if (templateName == null) {
+            System.out.println("Template for \"" + myPath + "\" not detected.");
+            return;
+        }
+        templateName = templateName.toLowerCase();
+
+        FileEditorManager.getInstance(project).openFile(viewPsiFile.getVirtualFile(), true);
+
+        final FileTemplate[] templates = FileTemplateManager.getDefaultInstance().getTemplates(FileTemplateManager.DEFAULT_TEMPLATES_CATEGORY);
+        FileTemplate template = null;
+        for (FileTemplate fileTemplate : templates) {
+            if (fileTemplate.isTemplateOfType(fileType) && fileTemplate.getName().toLowerCase().equals(templateName)) {
+                template = fileTemplate;
+                break;
+            }
+        }
+
+        if (template != null && viewPsiFile.getViewProvider().getDocument() != null) {
+            final Properties properties = FileTemplateManager.getDefaultInstance().getDefaultProperties();
+            template.setLiveTemplateEnabled(true);
+            template.setReformatCode(true);
+            try {
+                viewPsiFile.getViewProvider().getDocument().insertString(0, template.getText(properties));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getFileTemplateName(FileType fileType) {
+        if (fileType == PhpFileType.INSTANCE) {
+            return "PHP File";
+        } else if (fileType == SmartyFileType.INSTANCE) {
+            return "Smarty File";
+        } else {
+            try {
+                Class.forName("com.jetbrains.twig.TwigFileType");
+                if (fileType == TwigFileType.INSTANCE) {
+                    return "Twig File";
+                }
+            } catch (ClassNotFoundException e) {
+                return null;
+            }
+        }
+        return null;
     }
 }
