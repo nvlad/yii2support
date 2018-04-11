87a6d4e6f4318d9e67d5a30f9d3e6b0f0870e087
NVlad <wildvampir@gmail.com>
2018-04-11 19:05:12 +0300

    apply migrations

--- src/com/nvlad/yii2support/migrations/actions/MigrateUpAction.java
+++ src/com/nvlad/yii2support/migrations/actions/MigrateUpAction.java
@@ -4,17 +4,20 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.ui.AnActionButton;
 import com.nvlad.yii2support.migrations.MigrationManager;
+import com.nvlad.yii2support.migrations.entities.Migration;
+import com.nvlad.yii2support.migrations.entities.MigrationStatus;
 import com.nvlad.yii2support.migrations.ui.MigrationPanel;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
+import java.util.Enumeration;
 import java.util.Set;
 
 @SuppressWarnings("ComponentNotRegistered")
 public class MigrateUpAction extends AnActionButton {
     public MigrateUpAction() {
-        super("Migrate Up", AllIcons.Actions.Rerun);
+        super("Migrate Up", AllIcons.Actions.Execute);
     }
 
     @Override
@@ -23,15 +26,44 @@ public class MigrateUpAction extends AnActionButton {
         JTree tree = panel.getTree();
 
         if (tree.getSelectionModel().getSelectionCount() > 0) {
-            TreePath leadSelectionPath =  tree.getLeadSelectionPath();
-            if (leadSelectionPath == null)  {
+            TreePath leadSelectionPath = tree.getLeadSelectionPath();
+            if (leadSelectionPath == null) {
                 return;
             }
 
-            DefaultMutableTreeNode object = (DefaultMutableTreeNode) leadSelectionPath.getLastPathComponent();
-            if (object.getUserObject() instanceof String) {
+            DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) leadSelectionPath.getLastPathComponent();
+            Object userObject = treeNode.getUserObject();
+            if (userObject instanceof String) {
                 MigrationManager manager = MigrationManager.getInstance(anActionEvent.getProject());
-                Set<String> migrations = manager.migrateUp((String) object.getUserObject(), 1);
+                Set<String> migrations = manager.migrateUp((String) userObject, 0);
+            }
+
+            if (userObject instanceof Migration) {
+                Migration migration = (Migration) userObject;
+                if (migration.status == MigrationStatus.Success) {
+                    return;
+                }
+
+                int count = 0;
+                Enumeration migrationEnumeration = treeNode.getParent().children();
+                while (migrationEnumeration.hasMoreElements()) {
+                    Migration tmp = (Migration) ((DefaultMutableTreeNode) migrationEnumeration.nextElement()).getUserObject();
+                    if (migration.status != MigrationStatus.Success) {
+                        count++;
+                    }
+
+                    if (tmp == migration) {
+                        break;
+                    }
+                }
+
+                if (count == 0) {
+                    return;
+                }
+
+                MigrationManager manager = MigrationManager.getInstance(anActionEvent.getProject());
+                Object parentUserObject = ((DefaultMutableTreeNode) treeNode.getParent()).getUserObject();
+                Set<String> migrations = manager.migrateUp((String) parentUserObject, count);
             }
 
 //
