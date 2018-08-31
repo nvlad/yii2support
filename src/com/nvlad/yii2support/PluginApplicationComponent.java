fdecd2fe1a42153d51c6072d7ee3ad6dc67738e3
NVlad <wildvampir@gmail.com>
2018-08-31 11:37:06 +0300

    migrate to sentry

--- src/com/nvlad/yii2support/PluginApplicationComponent.java
+++ src/com/nvlad/yii2support/PluginApplicationComponent.java
@@ -12,22 +12,30 @@ import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.Consumer;
-import com.rollbar.api.payload.Payload;
-import com.rollbar.api.payload.data.Person;
-import com.rollbar.notifier.Rollbar;
-import com.rollbar.notifier.config.Config;
-import com.rollbar.notifier.config.ConfigBuilder;
-import com.rollbar.notifier.sender.listener.SenderListener;
-import com.rollbar.notifier.sender.result.Response;
+//import com.rollbar.api.payload.Payload;
+//import com.rollbar.api.payload.data.Person;
+//import com.rollbar.notifier.Rollbar;
+//import com.rollbar.notifier.config.Config;
+//import com.rollbar.notifier.config.ConfigBuilder;
+//import com.rollbar.notifier.sender.listener.SenderListener;
+//import com.rollbar.notifier.sender.result.Response;
+import io.sentry.DefaultSentryClientFactory;
+import io.sentry.Sentry;
+import io.sentry.SentryClient;
+import io.sentry.SentryClientFactory;
+import io.sentry.connection.EventSendCallback;
+import io.sentry.context.Context;
+import io.sentry.event.Event;
+import io.sentry.event.UserBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
-import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+//import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
 public class PluginApplicationComponent implements ApplicationComponent {
     private static final PluginId PLUGIN_ID = PluginId.getId("com.yii2support");
-    private static Rollbar rollbar;
+//    private static Rollbar rollbar;
     private final Object lock;
 
     public PluginApplicationComponent() {
@@ -74,95 +82,122 @@ public class PluginApplicationComponent implements ApplicationComponent {
 
     private SubmittedReportInfo reportInfo;
     public void submitErrorReport(Throwable error, String description, Consumer<SubmittedReportInfo> consumer) {
-        Rollbar rollbar = getRollbar();
-        Thread thread = new Thread(() -> {
-            synchronized(lock) {
-                reportInfo = null;
-                rollbar.error(error, description);
-                while(reportInfo == null) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+//        Sentry.init("https://e467771bed0c4bb2a39be4c46064d5e5@sentry.io/1271822");
+//        Sentry.init(new DefaultSentryClientFactory());
+//        Sentry.getContext();
+//        Sentry.capture(error);
+
+        SentryClient sentry = SentryClientFactory.sentryClient("https://e467771bed0c4bb2a39be4c46064d5e5@sentry.io/1271822");
+        Context context = sentry.getContext();
+        context.setUser(new UserBuilder().setId("nvlad").setUsername("NVlad").build());
+        sentry.addEventSendCallback(new EventSendCallback() {
+            @Override
+            public void onFailure(Event event, Exception exception) {
+                consumer.consume(new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED));
             }
 
-            consumer.consume(reportInfo);
+            @Override
+            public void onSuccess(Event event) {
+                consumer.consume(new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE));
+            }
         });
-        thread.start();
-    }
-
-    private Rollbar getRollbar() {
-        if (rollbar == null) {
-            Config config = rollbarConfig();
-            config.sender().addListener(new SenderListener() {
-                @Override
-                public void onResponse(Payload payload, Response response) {
-                    synchronized(lock) {
-                        reportInfo = new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE);
-                        lock.notifyAll();
-                    }
-
-                    if (response.getResult().isError()) {
-                        return;
-                    }
-
-                    showNotification("Report sent. Thanks for contributing.", NotificationType.INFORMATION);
-                }
-
-                @Override
-                public void onError(Payload payload, Exception e) {
-                    synchronized(lock) {
-                        reportInfo = new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED);
-                        lock.notifyAll();
-                    }
-
-                    showNotification("Error report not sent. Try late.", NotificationType.ERROR);
-                }
-
-                private void showNotification(String content, NotificationType notificationType) {
-                    final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PLUGIN_ID);
-                    if (plugin == null) {
-                        return;
-                    }
-
-                    NotificationGroup group = new NotificationGroup(plugin.getName(), NotificationDisplayType.STICKY_BALLOON, true);
-                    Notification notification = group.createNotification(
-                            plugin.getName(), content, notificationType, NotificationListener.URL_OPENING_LISTENER
-                    );
-                    Notifications.Bus.notify(notification);
-                }
-            });
-            rollbar = Rollbar.init(config);
-        }
-
-        return rollbar;
-    }
-
-    private Config rollbarConfig() {
-        final ConfigBuilder config = withAccessToken("7b5e3f2f1e3d4084869f7fff29f87688");
-        final PluginGlobalSettings settings = PluginGlobalSettings.getInstance();
-        final ApplicationInfoImpl applicationInfo = (ApplicationInfoImpl) ApplicationInfo.getInstance();
-        String environment = applicationInfo.getApiVersion();
-        environment = environment + " (" + applicationInfo.getFullApplicationName() + ")";
-
-        config
-                .person(() -> new Person.Builder().id(settings.uuid).username(settings.username).build())
-                .environment(environment)
-                .codeVersion("a4299cd21bfc5ff0d51c44b708e33ba08e09a081")
-                .framework(SystemInfo.JAVA_RUNTIME_VERSION)
-                .platform(SystemInfo.OS_NAME + " " + SystemInfo.OS_VERSION + " (" + SystemInfo.OS_ARCH + ")");
 
         final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PLUGIN_ID);
-        if (plugin == null) {
-            return config.build();
+        if (plugin != null) {
+            sentry.setRelease(plugin.getVersion());
         }
 
-        config.context(plugin::getVersion);
+        sentry.sendException(error);
 
-        return config.build();
+//        Rollbar rollbar = getRollbar();
+//        Thread thread = new Thread(() -> {
+//            synchronized(lock) {
+//                reportInfo = null;
+//                rollbar.error(error, description);
+//                while(reportInfo == null) {
+//                    try {
+//                        lock.wait();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//            consumer.consume(reportInfo);
+//        });
+//        thread.start();
     }
+
+//    private Rollbar getRollbar() {
+//        if (rollbar == null) {
+//            Config config = rollbarConfig();
+//            config.sender().addListener(new SenderListener() {
+//                @Override
+//                public void onResponse(Payload payload, Response response) {
+//                    synchronized(lock) {
+//                        reportInfo = new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE);
+//                        lock.notifyAll();
+//                    }
+//
+//                    if (response.getResult().isError()) {
+//                        return;
+//                    }
+//
+//                    showNotification("Report sent. Thanks for contributing.", NotificationType.INFORMATION);
+//                }
+//
+//                @Override
+//                public void onError(Payload payload, Exception e) {
+//                    synchronized(lock) {
+//                        reportInfo = new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.FAILED);
+//                        lock.notifyAll();
+//                    }
+//
+//                    showNotification("Error report not sent. Try late.", NotificationType.ERROR);
+//                }
+//
+//                private void showNotification(String content, NotificationType notificationType) {
+//                    final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PLUGIN_ID);
+//                    if (plugin == null) {
+//                        return;
+//                    }
+//
+//                    NotificationGroup group = new NotificationGroup(plugin.getName(), NotificationDisplayType.STICKY_BALLOON, true);
+//                    Notification notification = group.createNotification(
+//                            plugin.getName(), content, notificationType, NotificationListener.URL_OPENING_LISTENER
+//                    );
+//                    Notifications.Bus.notify(notification);
+//                }
+//            });
+//            rollbar = Rollbar.init(config);
+//        }
+//
+//        return rollbar;
+//    }
+//
+//    private Config rollbarConfig() {
+//        final ConfigBuilder config = withAccessToken("469a7c05fd5c438f94f9820923434b39");
+//        final PluginGlobalSettings settings = PluginGlobalSettings.getInstance();
+//        final ApplicationInfoImpl applicationInfo = (ApplicationInfoImpl) ApplicationInfo.getInstance();
+//        String environment = applicationInfo.getApiVersion();
+//        environment = environment + " (" + applicationInfo.getFullApplicationName() + ")";
+//
+//        config
+//                .person(() -> new Person.Builder().id(settings.uuid).username(settings.username).build())
+//                .environment(environment)
+//                .codeVersion("v0.10.56.18")
+//                .framework(SystemInfo.JAVA_RUNTIME_VERSION)
+//                .platform(SystemInfo.OS_NAME + " " + SystemInfo.OS_VERSION + " (" + SystemInfo.OS_ARCH + ")");
+//
+//        final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PLUGIN_ID);
+//        if (plugin == null) {
+//            return config.build();
+//        }
+//
+//        config.context(plugin::getVersion);
+//
+//        return config.build();
+//    }
 
     public static PluginApplicationComponent getInstance() {
         return ApplicationManager.getApplication().getComponent(PluginApplicationComponent.class);
