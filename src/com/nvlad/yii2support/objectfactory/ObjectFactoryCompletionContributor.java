2a5b059b58af9896220a84abf644b0002f95d9a8
NVlad <wildvampir@gmail.com>
2020-10-28 20:01:30 +0300

    code completion

--- src/com/nvlad/yii2support/objectfactory/ObjectFactoryCompletionContributor.java
+++ src/com/nvlad/yii2support/objectfactory/ObjectFactoryCompletionContributor.java
@@ -1,16 +1,29 @@
 package com.nvlad.yii2support.objectfactory;
 
+import com.intellij.codeInsight.completion.AutoCompletionContext;
+import com.intellij.codeInsight.completion.AutoCompletionDecision;
+import com.intellij.codeInsight.completion.CompletionInitializationContext;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
-import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.elements.ArrayAccessExpression;
+import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.nvlad.yii2support.common.Patterns;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ObjectFactoryCompletionContributor extends com.intellij.codeInsight.completion.CompletionContributor {
     public ObjectFactoryCompletionContributor() {
         extend(CompletionType.BASIC, ElementPattern(), new ObjectFactoryCompletionProvider());
+    }
+
+    @Nullable
+    @Override
+    public AutoCompletionDecision handleAutoCompletionPossibility(@NotNull AutoCompletionContext context) {
+        return super.handleAutoCompletionPossibility(context);
     }
 
     @Override
@@ -24,15 +37,15 @@ public class ObjectFactoryCompletionContributor extends com.intellij.codeInsight
     }
 
     private static ElementPattern<PsiElement> ElementPattern() {
-
         return PlatformPatterns.psiElement()
-                .withParent(PlatformPatterns.psiElement(StringLiteralExpression.class)
-                        .withParent(PlatformPatterns.or(
-                                PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class),
-                                PlatformPatterns.psiElement().withParent(ArrayAccessExpression.class),
-                                Patterns.withHashKey()
-                                        .withParent(PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class))
-                        )));
-
+            .withParent(PlatformPatterns.psiElement(StringLiteralExpression.class)
+                .withParent(PlatformPatterns.or(
+                    PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class),
+                    PlatformPatterns.psiElement().withParent(ArrayAccessExpression.class),
+                    PlatformPatterns.psiElement(PhpPsiElement.class),
+                    Patterns.withHashKey().withParent(PlatformPatterns.psiElement().withParent(ArrayCreationExpression.class))
+                    )
+                )
+            );
     }
 }
