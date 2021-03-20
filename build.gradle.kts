3d0af47701f8ce2f9a5bde70357998894d23ef65
GloooM <glooom@xtlt.ru>
2021-03-20 18:10:18 +0400

    My build/debug config

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ version = pluginVersion
 val platformPluginsAssociation = hashMapOf<String, String>()
 platformPluginsAssociation["2019.1.4"] = "com.jetbrains.php:191.8026.56, org.jetbrains.plugins.phpstorm-remote-interpreter:191.5849.22, com.jetbrains.twig:191.6183.95"
 platformPluginsAssociation["2020.2.3"] = "com.jetbrains.php:202.7660.42, org.jetbrains.plugins.phpstorm-remote-interpreter:202.6397.59, com.jetbrains.twig:202.6397.21"
+platformPluginsAssociation["2020.3.3"] = "com.jetbrains.php:203.7717.11, org.jetbrains.plugins.phpstorm-remote-interpreter:203.5981.155, com.jetbrains.twig:203.6682.75"
 val bundledPlugins = "DatabaseTools, webDeployment, CSS, terminal, coverage, java-i18n, remote-run, properties"
 
 val platformPlugins = platformPluginsAssociation[platformVersion] + ", $bundledPlugins"
@@ -70,11 +71,11 @@ sourceSets {
             srcDirs("resources")
         }
     }
-    test {
+    /*test {
         java {
             srcDirs("tests")
         }
-    }
+    }*/
 }
 
 tasks {
@@ -108,23 +109,23 @@ tasks {
 //        )
     }
 
-    test {
+   // test {
 //        useJUnitPlatform()
-        reports {
-            junitXml.isEnabled = true
-        }
-    }
+     //   reports {
+       //     junitXml.isEnabled = true
+        //}
+   // }
 
-    runPluginVerifier {
-        ideVersions(pluginVerifierIdeVersions)
-    }
+   // runPluginVerifier {
+     //   ideVersions(pluginVerifierIdeVersions)
+    //}
 
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token(System.getenv("PUBLISH_TOKEN"))
+    //publishPlugin {
+      //  dependsOn("patchChangelog")
+        //token(System.getenv("PUBLISH_TOKEN"))
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://jetbrains.org/intellij/sdk/docs/tutorials/build_system/deployment.html#specifying-a-release-channel
-        channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
-    }
+        //channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
+    //}
 }
