3a971768797d97d524a0f96cdb5469a8684635b4
GloooM <glooom@xtlt.ru>
2021-03-23 18:57:31 +0400

    More specific pattern for completion

--- src/com/nvlad/yii2support/configurations/YiiAppCompletionContributor.java
+++ src/com/nvlad/yii2support/configurations/YiiAppCompletionContributor.java
@@ -3,10 +3,11 @@ package com.nvlad.yii2support.configurations;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.patterns.PlatformPatterns;
+import com.jetbrains.php.lang.psi.elements.FieldReference;
 
 public class YiiAppCompletionContributor extends CompletionContributor {
     public YiiAppCompletionContributor() {
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new YiiAppCompletionProvider());
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement().withParent(FieldReference.class), new YiiAppCompletionProvider());
     }
 
 }
